c0da5d43c3a7e60f925a61bdb892a03b857d940f
yahya <cherifyahya069@gmail.com>
2023-09-07 19:58:14 -0700

    file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, message, 59);
+return (1);
+}
