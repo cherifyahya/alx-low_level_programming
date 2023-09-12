181fde0662c7fce9e14bf316cdef66ab3e1acecf
yahya <cherifyahya069@gmail.com>
2023-09-12 11:10:08 -0700

    c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet
+ */
+
+void print_alphabet(void)
+{
+	char ltr;
+
+	for (ltr = 'a'; ltr <= 'z'; ltr++)
+		_putchar(ltr);
+
+	_putchar('\n');
+}
+
