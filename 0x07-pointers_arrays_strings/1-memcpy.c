ad31fa8abc09761e841ecb6b6f30369be59b72bf
yahya <cherifyahya069@gmail.com>
2023-09-25 11:14:24 -0700

    zz

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,11 +5,11 @@
  *@src: source
  *@n: number of bytes
  *
- *Return: a pointer to dest
+ *Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
