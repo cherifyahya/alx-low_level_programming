4c5b935bc71c46695941fbe20cec2deadca94930
cherifyahya <cherifyahya069@gmail.com>
2023-10-24 14:07:27 -0700

    zz

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverse a l.l
+ * @head: pointer
+ * Return: pointer
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *p = NULL;
+	listint_t *next = NULL;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = p;
+		p = *head;
+		*head = next;
+	}
+
+	*head = p;
+
+	return (*head);
+}
